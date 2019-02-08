9c290e8c451f73f0e2200fdc4e877804fa0e6fb5
Ryan Thally <rthally94@me.com>
2019-02-08 15:38:28 -0500

    Initial Commit

---
+++ Tradition Keepers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tradition Keepers
+//
+//  Created by Ryan Thally on 2/8/19.
+//  Copyright © 2019 East Tennessee State Univeristy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Tradition KeepersTests/Tradition_KeepersTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tradition_KeepersTests.swift
+//  Tradition KeepersTests
+//
+//  Created by Ryan Thally on 2/8/19.
+//  Copyright © 2019 East Tennessee State Univeristy. All rights reserved.
+//
+
+import XCTest
+@testable import Tradition_Keepers
+
+class Tradition_KeepersTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
