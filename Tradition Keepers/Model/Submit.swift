3b542f70636bf997351b5ef2cbf4e1925b37cf07
Ryan Thally <rthally94@gmail.com>
2019-04-10 12:37:42 -0400

    Added Submit class template

---
+++ Tradition Keepers/Model/Submit.swift
@@ -0,0 +1,28 @@
+//
+//  Submit.swift
+//  Tradition Keepers
+//
+//  Created by Ryan Thally on 4/10/19.
+//  Copyright © 2019 East Tennessee State Univeristy. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+/// Provides functions for submitting traditions.
+class Submit {
+    /// Submits a tradition using the camera for verification.
+    static func WithCamera() {
+    
+    }
+    
+    /// Submits a tradition using a prompt for verification.
+    static func WithPrompt() {
+    
+    }
+    
+    /// Shows an Alert prompting for submission. Shows optional UIImage if WithCamera() is used.
+    private static func ShowPrompt() {
+        
+    }
+}
