074e50f4f07be2c7cae849df80dd80e8e5eb515e
Ryan Thally <rthally94@gmail.com>
2019-04-17 15:02:30 -0400

    Making Submission View

--- Tradition Keepers/3.0 - Submit/SubmitViewController.swift
+++ Tradition Keepers/3.0 - Submit/SubmitViewController.swift
@@ -9,21 +9,62 @@
 import UIKit
 
 class SubmitViewController: UIViewController {
+    @IBOutlet weak var TraditionNameLabel: UILabel!
+    @IBOutlet weak var TraditionRequirementLabel: UILabel!
+    @IBOutlet weak var ApproveButton: UIButton!
+    
+    @IBOutlet weak var LocationLabel: UILabel!
+    @IBOutlet weak var SubmissionImage: UIImageView!
+    
+    @IBOutlet weak var UserNameLabel: UILabel!
+    @IBOutlet weak var UserDateLabel: UILabel!
+    
+    @IBOutlet weak var ApprovalLabel: UILabel!
+    @IBOutlet weak var ApprovalDateLabel: UILabel!
+    
+    @IBOutlet weak var DenyBarButton: UIBarButtonItem!
+    @IBOutlet weak var DenyButton: UIButton!
+    @IBOutlet weak var SubmitBarButton: UIBarButtonItem!
+    
     var imagePicker: ImagePicker!
+    var tradition: Tradition!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         imagePicker = ImagePicker(presentationController: self, delegate: self)
+        imagePicker.present(from: UIView())
         // Do any additional setup after loading the view.
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        imagePicker.present(from: UIView())
+    private func prepareView() {
+        
+        // Only allow button presses for staff/admin
+        switch User.current.permission {
+        case .staff, .admin:
+            ApproveButton.isEnabled = true
+            DenyBarButton.isEnabled = true
+            DenyButton.isHidden = false
+        default:
+            ApproveButton.isEnabled = false
+            DenyBarButton.isEnabled = false
+            DenyButton.isHidden = true
+        }
+        
+        // Only show button for .pending and .complete state
+        switch tradition.submission.status {
+        case .pending, .complete:
+            ApproveButton.isHidden = false
+            SubmitBarButton.title = "Approve"
+        default:
+            ApproveButton.isHidden = true
+        }
+    }
+
+    @IBAction func SubmitButtonPressed(_ sender: Any) {
+        
     }
     
-
     /*
     // MARK: - Navigation
 
